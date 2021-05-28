17f07068611926632de3bc5f9aa6b615822548c9
dpeter99 <dpeter99@gmail.com>
2021-05-28 10:40:11 +0200

    Added Curios and JEI

--- src/main/java/com/dpeter99/arcanerituals/ArcaneRituals.java
+++ src/main/java/com/dpeter99/arcanerituals/ArcaneRituals.java
@@ -9,10 +9,13 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import top.theillusivec4.curios.api.SlotTypeMessage;
 
 @Mod("arcanerituals")
 public class ArcaneRituals {
@@ -25,11 +28,19 @@ public class ArcaneRituals {
     public ArcaneRituals() {
         ARRegistry.initialize();
 
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+
         MinecraftForge.EVENT_BUS.register(this);
 
         LOGGER.error(TestKt.INSTANCE.getTEST());
 
 
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+        // some example code to dispatch IMC to another mod
+        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("ring").build());
     }
 
     public static ResourceLocation location(String path)
@@ -42,6 +53,7 @@ public class ArcaneRituals {
     public void onAddReloadListenerEvent(AddReloadListenerEvent event){
         event.addListener(new MobBloodManager());
     }
+
 
 
     public static final ItemGroup TAB = new ItemGroup("arcane_rituals") {
